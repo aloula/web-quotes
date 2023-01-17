614b4b73b0064a7510b4eaa048d42a6284ae0d1f
Alexsander Loula <asloula@rd.com.br>
2023-01-16 23:06:31 -0300

    alterado readme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Testa se o autor mestre foi citado', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Cespedes/i);
   expect(linkElement).toBeInTheDocument();
 });
